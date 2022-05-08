835dc2af1708206554951ff0cec101f4b8ceb787
Michael MacInnis <Michael.P.MacInnis@gmail.com>
2022-05-08 02:34:28 -0400

    Add top-level build.sh.

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Figure out where we are.
+Dir=$(dirname -- $(readlink -f -- "$0"))
+
+cd "$Dir"
+
+go build ./cmd/client
+go build ./cmd/mux
+go build ./cmd/server
+
+sudo cp client /usr/local/bin/summit-client
+sudo cp mux /usr/local/bin/summit-mux
+
+./cmd/mux/build.sh
